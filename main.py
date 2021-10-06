797abd2db1d8eb70ee3a2967a0b2e261dcef6070
Weslley Borges <weslley15bs@gmail.com>
2021-10-06 13:16:48 -0300

    DEP: Add gunicorn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import nltk
-nltk.download('all')
+# import nltk
+# nltk.download('all')
 from src.services.chat.chat_processor import get_best_context
 from flask import Flask, request
 import os
@@ -17,4 +17,4 @@ def login():
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port)
+  app.run(port=port, debug=True)
